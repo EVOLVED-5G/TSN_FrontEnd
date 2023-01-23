20d566145834a70bc82767f42630e4b44d059477
nanitebased <bgg@uma.es>
2023-01-23 10:52:53 +0100

    Add, render README.md. Add LICENSE

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from flask_misaka import Misaka
 
 app = Flask(__name__)
 
-
 from front_end import bp as frontEndApi
 app.register_blueprint(frontEndApi, url_prefix='/api/v1', name='frontEndApp')
+
+Misaka(app)
+with open('README.md', 'r', encoding='utf-8') as file:
+    readme = file.read()
+
+@app.route('/')
+def index():
+    return render_template('index.html', mkd=readme)
 
 
 if __name__ == '__main__':
