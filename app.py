cd81f325b4acbd0cbd153f9e4b6887e115195c9a
nanitebased <bgg@uma.es>
2023-03-01 14:27:29 +0100

    Parse config as json

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template
 from flask_misaka import Misaka
 from back_end import ConfigurationHandler
 from capif import maybePublishApi
-import yaml
+import json
 
 app = Flask(__name__)
 
 from front_end import bp as frontEndApi
 app.register_blueprint(frontEndApi, url_prefix='/tsn/api/v1', name='frontEndApi')
 
-with open('config.yml', 'r', encoding='utf-8') as file:
-    data = yaml.safe_load(file)
+with open('config.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
     ConfigurationHandler.SetBackEnd(data['BackEnd'])
 
 Misaka(app, tables=True)
