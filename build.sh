6523615503db34c84b7dd9f6f6e1f3e447ca0599
nanitebased <bgg@uma.es>
2023-03-01 13:18:16 +0000

    Use port value in config to generate Dockerfile

--- build.sh
+++ build.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
+echo "Checking for 'jq' availability..."
+jq --version
+if [ $? -ne 0 ]; then
+	echo "'jq' command is not available. Please install and re-run this script."
+	echo "See 'https://stedolan.github.io/jq/download/' for more information."
+	exit
+fi
+
+port=$(jq .FrontEnd.Port config.json)
+
+echo ""
+echo "Generating Dockerfile (FrontEnd Port in config.json is $port)..."
+
+cp Dockerfile.Template Dockerfile
+sed -i "s/{{PORT}}/$port/" Dockerfile
+
+echo ""
+echo "Building docker image..."
+
 docker build -t tsn_af .
+
+echo "Done"
