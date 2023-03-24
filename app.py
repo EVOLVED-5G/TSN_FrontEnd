fd510df38c3fe2d4aa8630a8691d0c989233cbd6
nanitebased <bgg@uma.es>
2023-03-24 12:09:34 +0100

    Add security configuration, read CAPIF public key

--- app.py
+++ app.py
@@ -6,22 +6,37 @@ import json
 
 app = Flask(__name__)
 
-from front_end import bp as frontEndApi
-app.register_blueprint(frontEndApi, url_prefix='/tsn/api/v1', name='frontEndApi')
-
 with open('config.json', 'r', encoding='utf-8') as file:
     config = json.load(file)
     ConfigurationHandler.SetBackEnd(config['BackEnd'])
+    app.config["CAPIF_ENABLED"] = config['CAPIF']['Enabled']
+    app.config["CAPIF_SECURITY_ENABLED"] = config['CAPIF']['SecurityEnabled']
 
 Misaka(app, tables=True)
 with open('README.md', 'r', encoding='utf-8') as file:
     readme = file.read()
 
+if app.config["CAPIF_ENABLED"]:
+    maybePublicKey = maybePublishApi(config)
+
+    if app.config["CAPIF_SECURITY_ENABLED"]:
+        if maybePublicKey is None:
+            print("Security enabled, but unable to retrieve public key. Aborting.")
+            exit()
+        else:
+            from flask_jwt_extended import JWTManager
+            JWTManager(app)
+
+            app.config["JWT_ALGORITHM"] = 'RS256'
+            app.config["JWT_PUBLIC_KEY"] = maybePublicKey
+
+
+from front_end import bp as frontEndApi
+app.register_blueprint(frontEndApi, url_prefix='/tsn/api/v1', name='frontEndApi')
+
 @app.route('/')
 def index():
     return render_template('index.html', mkd=readme)
-
-maybePublishApi(config)
 
 if __name__ == '__main__':
     app.run()
