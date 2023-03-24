24912a9f9ba7efae009f77f672843326c209b2ea
nanitebased <bgg@uma.es>
2023-03-24 12:55:51 +0100

    Check Authorization header before processing requests

--- front_end/routes.py
+++ front_end/routes.py
@@ -1,7 +1,8 @@
 from typing import Dict, List
-from flask import jsonify, request
+from flask import jsonify, request, current_app
 from front_end import bp
 from back_end import ProfileHandler, ConfigurationHandler
+from flask_jwt_extended import jwt_required, get_jwt
 
 def validate(data: Dict, expected: List[str]):
     keys = data.keys()
@@ -11,8 +12,23 @@ def validate(data: Dict, expected: List[str]):
             missing.append(key)
     return missing
 
+def isAuthorized() -> bool:
+    if current_app.config["CAPIF_SECURITY_ENABLED"]:
+        # If the token exists, but is invalid, @jwt_required will handle the error. This can be confirmed by commenting
+        # @jwt_required, importing verify_jwt_in_request and adding a breakpoint before processing the request. Calling
+        # verify_jwt_in_request with a too short Bearer will raise "Not enough segments", the same as when using
+        # @jwt_required directly.
+
+        return len(get_jwt()) != 0
+    else:
+        return True
+
+
 @bp.route('/profile', methods=['GET'])
+@jwt_required(optional=True)
 def profile():
+    if not isAuthorized(): return "403 Forbidden", 403
+
     name = request.args.get('name', None)
     if name is None:
         return jsonify(
@@ -24,7 +40,10 @@ def profile():
         )
 
 @bp.route('/apply', methods=['POST'])
+@jwt_required(optional=True)
 def apply():
+    if not isAuthorized(): return "403 Forbidden", 403
+
     data = request.json
 
     missing = validate(data, ['profile', 'identifier', 'overrides'])
@@ -45,7 +64,10 @@ def apply():
         }), 400
 
 @bp.route('/clear', methods=['POST'])
+@jwt_required(optional=True)
 def clear():
+    if not isAuthorized(): return "403 Forbidden", 403
+
     data = request.json
 
     missing = validate(data, ['identifier', 'token'])
